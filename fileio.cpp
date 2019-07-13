c297f80f5b4813c75d80c3de831f5015b869b05f
Grayson <barrowsgrax@gmail.com>
2019-07-13 17:18:19 -0400

    First Commit

---
+++ fileio.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+ 
+ int main () {  
+ofstream file;
+string Input = "";
+cout << "What is your name?\n";
+cin >> Input;
+file.open ("text.txt");
+file << Input <<endl;
+file.close();
+return 0;
+}
